858614f02ca7cda3b323040df43a145ffcc1e76c
masv3871 <masv@sunet.se>
2021-07-02 10:14:10 +0200

    Humantouch release v0.1.16

--- helper.go
+++ helper.go
@@ -2,12 +2,10 @@ package humantouch
 
 import (
 	"math/rand"
-	"time"
 )
 
 func randomGender() string {
-	random := rand.New(rand.NewSource(time.Now().Unix()))
-	switch r := random.Intn(2); r {
+	switch r := rand.Intn(2); r {
 	case 0:
 		return GenderFemale
 	case 1:
